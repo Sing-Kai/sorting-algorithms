2275bf2ec9a6b301d185827f22a3ff3f3832cf66
Sing-Kai Chiu <singkai.chiu@gmail.com>
2020-10-28 21:15:33 +0000

    first commit

---
+++ InsertionSort.cs
@@ -0,0 +1,18 @@
+
+using System;
+
+namespace Sorting
+{
+
+    public static class Insertion
+    {
+
+        public static void Sort(int[] arr)
+        {
+            for(var i= 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] arr = new int[15] {2, 5, -4, 11, 0, 15, 22, 67, 51, 6, 65, 23, 89, 1, 40};
+            Insertion.Sort(arr);
+        }
+    }
+}
